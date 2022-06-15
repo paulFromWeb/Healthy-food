bfe8254ca1e464b96a204d7af566a84ecc47c7e1
Pavel <pavel.krasinskiy@bk.ru>
2022-06-15 10:16:52 +0800

    m

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults({
-    static: '../newPr2',
+    static: './build',
 });
 
 const PORT = process.env.PORT || 3000;
